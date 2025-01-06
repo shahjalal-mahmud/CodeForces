634c5f232aedfa1d8894e36d59c4803c54187dea
AbuBokkar95 <mahmud.nubtk@gmail.com>
2025-01-07 02:10:04 +0600

    6 min

--- Rank800/A_Nearly_Lucky_Number.cpp
+++ Rank800/A_Nearly_Lucky_Number.cpp
@@ -1,6 +1,27 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
- return 0;
+    long long n;
+    cin >> n;
+    int count = 0;
+    while (n > 0)
+    {
+        long long digit = n % 10;
+        if (digit == 4 || digit == 7)
+        {
+            count++;
+        }
+        n = n / 10;
+    }
+    if (count == 4 || count == 7)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+
+    return 0;
 }
