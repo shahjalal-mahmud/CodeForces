e7649b5415730e1ac98c830ae5ee2d3a5726d527
AbuBokkar95 <mahmud.nubtk@gmail.com>
2025-01-06 21:59:52 +0600

    Done soldier banana

--- Rank800/SoldierandBananas.cpp
+++ Rank800/SoldierandBananas.cpp
@@ -1,6 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
- return 0;
+    int k, n, w;
+    cin >> k >> n >> w;
+    int borrow = 0;
+    int price = 0;
+    for (int i = 1; i <= w; i++)
+    {
+        price += (i * k);
+    }
+    if (n < price)
+    {
+        borrow = price - n;
+        cout << borrow;
+    }
+    else
+    {
+        cout << 0;
+    }
+    return 0;
 }
