d832a1f3c4da4342a0c3a6a027193df06886c2fd
AbuBokkar95 <mahmud.nubtk@gmail.com>
2025-01-07 02:20:18 +0600

    9 min

---
+++ Rank800/A_Anton_and_Danik.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int a = 0, d = 0;
+    for (char ch : s)
+    {
+        if (ch == 'A')
+        {
+            a++;
+        }
+        else
+        {
+            d++;
+        }
+    }
+    if (a > d)
+    {
+        cout << "Anton";
+    }
+    else if (a < d)
+    {
+        cout << "Danik";
+    }
+    else
+    {
+        cout << "Friendship";
+    }
+
+    return 0;
+}
