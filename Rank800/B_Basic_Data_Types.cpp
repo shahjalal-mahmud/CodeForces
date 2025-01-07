894bc27da68fd12d8d82d8f948704945c1987a78
AbuBokkar95 <mahmud.nubtk@gmail.com>
2025-01-07 15:25:45 +0600

    10 min

---
+++ Rank800/B_Basic_Data_Types.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<cstdio>
+using namespace std;
+int main()
+{
+    int a;
+    long long b;
+    char c;
+    float d;
+    double e;
+    cin >> a >> b >> c >> d >> e;
+    cout<< a << endl << b << endl << c << endl << d<< endl;
+    printf("%.1f", e);
+    return 0;
+}
