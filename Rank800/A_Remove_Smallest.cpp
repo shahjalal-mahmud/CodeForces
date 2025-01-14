570ce541f53aa2bd11d99110a3e4c702c081e34c
AbuBokkar95 <mahmud.nubtk@gmail.com>
2025-01-14 13:22:25 +0600

    15 min

---
+++ Rank800/A_Remove_Smallest.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        sort(arr.begin(), arr.end());
+        bool possible = true;
+        for (int i = n - 1; i >= 1; i--)
+        {
+            if (arr[i] - arr[i - 1] > 1)
+            {
+                possible = false;
+                break;
+            }
+        }
+        if (possible)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
