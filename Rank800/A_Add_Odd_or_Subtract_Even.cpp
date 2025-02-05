6f2def0af00a47152e57122f9354a3ea35469f27
AbuBokkar95 <mahmud.nubtk@gmail.com>
2025-02-05 23:28:05 +0600

    10 min

---
+++ Rank800/A_Add_Odd_or_Subtract_Even.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        int moves = 0;
+        if (a == b)
+        {
+            cout << "0" << endl;
+        }
+        else if (a < b)
+        {
+            if ((b - a) % 2 == 0)
+            {
+                cout << "2" << endl;
+            }
+            else
+            {
+                cout << "1" << endl;
+            }
+        }
+        else
+        {
+            if ((a - b) % 2 == 0)
+            {
+                cout << "1" << endl;
+            }
+            else
+            {
+                cout << "2" << endl;
+            }
+        }
+    }
+
+    return 0;
+}
