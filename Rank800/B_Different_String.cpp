6a5db09f85e0f20288a0a53052fb087be53870bb
AbuBokkar95 <mahmud.nubtk@gmail.com>
2025-02-19 23:29:18 +0600

    5 min

---
+++ Rank800/B_Different_String.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        string r = s;
+        sort(r.begin(), r.end());
+        if (r == s) {
+            reverse(r.begin(), r.end());
+            if (r == s) {
+                cout << "NO" << endl;
+            } else {
+                cout << "YES" << endl;
+                cout << r << endl;
+            }
+        } else {
+            cout << "YES" << endl;
+            cout << r << endl;
+        }
+    }
+    return 0;
+}
